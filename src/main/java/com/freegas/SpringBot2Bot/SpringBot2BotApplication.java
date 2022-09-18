a3fdace36de6e36329e8e14a585e8e0b38fde72f
freegastello <freegastello@gmail.com>
2022-09-18 18:38:28 +0300

    Первый коммит

---
+++ src/main/java/com/freegas/SpringBot2Bot/SpringBot2BotApplication.java
@@ -0,0 +1,13 @@
+package com.freegas.SpringBot2Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBot2BotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBot2BotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freegas/SpringBot2Bot/SpringBot2BotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freegas.SpringBot2Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBot2BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
